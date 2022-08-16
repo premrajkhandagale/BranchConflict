1f2bfa25680764f1e91da8f39535603ed20a9f0c
premrajkhandagale <premkhandagale07@gmail.com>
2022-08-16 13:04:54 +0530

    this is team lead branch

---
+++ src/main/java/com/example/demo/BranchConflictsApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BranchConflictsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BranchConflictsApplication.class, args);
+	}
+
+}
